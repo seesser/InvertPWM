5437f2e761daad9dede249aca0f59b4732625b59
seesser <seesser@gmail.com>
2017-06-18 10:36:47 -0500

    Fixed spelling error and added step down logic.
    Changed duty_cylce to frequency.  Added a logic to step down power by percent of current selected power at a picked degree.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import time
 from modules import cbpi
 from modules.core.props import Property
 from modules.core.hardware import ActorBase
+from modules.core.controller import KettleController
 
 try:
     import RPi.GPIO as GPIO
@@ -12,34 +13,66 @@ except Exception as e:
     print e
     pass
 
+@cbpi.controller
+class InvertLogic(KettleController):
+
+    TempDiff = Property.Number("# of Degrees From Target To Start", True, 0)
+    PowDiff = Property.Number("Reduce Power By What % Of Selected Power", True, 0)
+    
+    def stop(self):
+
+        super(KettleController, self).stop()
+        self.heater_off()
+
+    def run(self):
+        if self.TempDiff is None:
+            self.TempDiff = 2
+        if self.PwrRdt is None:
+            self.PwrRdt = 50
+        
+        while self.is_running():
+            if self.get_temp()  >= self.get_target_temp():
+                self.heater_off()
+            elif self.get_temp() < (self.get_target_temp()-int(self.TempDiff)):
+                self.heater_on(int(100-InvertPWM.power))
+            else:
+                self.heater_on(int(100-(InvertPWM.power + 
+                                ((100-InvertPWM.power) * 
+                                int(self.PowDiff)/100))))
+            self.sleep(1)
+            self.heater_off()
+
 @cbpi.actor       
 class InvertPWM(ActorBase):
 
     gpio = Property.Select("GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27])
-    duty_cylce = Property.Number("Frequency", configurable=True)
-
+    frequency = Property.Number("Cycles Per Second", configurable=True)
+    
     power = 0
     p = None
     stopped = True
-
+    
     def init(self):
         GPIO.setup(int(self.gpio), GPIO.OUT)
         GPIO.output(int(self.gpio), 1)
 
-    def on(self, power=None):
+    def on(self, power):
         self.stopped = False
         if self.p is None:
-            if self.duty_cylce is None:
-                self.duty_cylce = 60
-            self.p = GPIO.PWM(int(self.gpio), int(self.duty_cylce))
-            self.p.start(int(self.power))
-        self.p.ChangeDutyCycle(int(self.power))
+            if self.frequency is None:
+                self.frequency = 50
+            self.p = GPIO.PWM(int(self.gpio), int(self.frequency))
+            self.p.start(int(InvertPWM.power))
+        if power is not None:
+            self.p.ChangeDutyCycle(int(100-power))
+        else:
+            self.p.ChangeDutyCycle(int(InvertPWM.power))
 
     def set_power(self, power):
         if power is not None:
-            self.power = (100-power)
+            InvertPWM.power = (100-power)
         if self.stopped is False:
-            self.p.ChangeDutyCycle(int(self.power))
+            self.p.ChangeDutyCycle(int(InvertPWM.power))
 
     def off(self):
         self.stopped = True
